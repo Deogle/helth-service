3f712efbac23e142e6ded42ceffe48f8cee14836
Dylan Ogle <deogle20@gmail.com>
2024-11-12 15:46:58 -0600

    Added utility to prevent sending duplicate pubsub messages

--- api/src/lib/services/publish.ts
+++ api/src/lib/services/publish.ts
@@ -1,17 +1,37 @@
 import { PubSub } from "@google-cloud/pubsub";
 import logger from "../../util/logger";
 import { PubSubMessage } from "../../types/api";
+import { MessageService } from "./messages";
 
 // Create a new instance of the PubSub client
 const pubsub = new PubSub();
 const { PUBSUB_TOPIC: topicName } = process.env;
 
-// Function to publish a message to a topic
-async function publishMessage(
+//Function to prevent publishing a duplicate message
+async function checkIfDuplicateMessage(
   message: PubSubMessage
-): Promise<void> {
+): Promise<boolean> {
+  const messageHash = Buffer.from(JSON.stringify(message)).toString("base64");
+  if (!messageHash) {
+    throw new Error("Error hashing message");
+  }
+
+  const previousMessages = await MessageService.getMessages();
+  const seen = previousMessages.map((msg) => msg.hash).includes(messageHash);
+  if (seen) {
+    return true;
+  }
+  await MessageService.createSeenMessage(messageHash);
+  return false;
+}
+
+async function publishMessage(message: PubSubMessage): Promise<void> {
   if (!topicName) {
     throw new Error("Missing PUBSUB_TOPIC environment variable");
+  }
+  if (await checkIfDuplicateMessage(message)) {
+    logger.info("Ignoring duplicate message", { message });
+    return;
   }
   try {
     const topic = pubsub.topic(topicName);
