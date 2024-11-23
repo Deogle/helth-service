d3cc0ba62488de19eb63b6034881da2dbbfdb2f3
Dylan Ogle <deogle@ha.deogle.local>
2024-11-23 11:07:48 -0600

    Update docker scripts, fix linter error

--- discord-client/docker-push.sh
+++ discord-client/docker-push.sh
@@ -1,2 +1,6 @@
-docker tag discord-client:dev us-docker.pkg.dev/helth-service-test/helth-ar/client
-docker push us-docker.pkg.dev/helth-service-test/helth-ar/client 
+# docker tag discord-client:dev us-docker.pkg.dev/helth-service-test/helth-ar/client
+# docker push us-docker.pkg.dev/helth-service-test/helth-ar/client 
+
+docker tag discord-client:dev dylanogle/helth-discord-client:latest
+docker push dylanogle/helth-discord-client:latest
+
