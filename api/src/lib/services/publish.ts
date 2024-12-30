0fe8adeac333fd792c98be8d8b2005b85070c4fd
Dylan Ogle <deogle20@gmail.com>
2024-12-30 14:03:16 -0600

    fix typo

--- api/src/lib/services/publish.ts
+++ api/src/lib/services/publish.ts
@@ -9,7 +9,7 @@ async function publishMessage(message: PubSubMessage): Promise<void> {
     url: process.env.REDIS_URL!,
   });
 
-  client.on("error", (err) => console.log("redis client erro", err));
+  client.on("error", (err) => console.log("redis client error", err));
   await client.connect();
   await client.publish(PUBSUB_CHANNEL, JSON.stringify(message));
   logger.info("published message to redis pubsub");
